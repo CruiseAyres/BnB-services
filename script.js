d25d778678fdeb0dbf4c5b94804ddb09f4e19240
cruiseboy2006 <64339861+cruiseboy2006@users.noreply.github.com>
2020-08-10 00:05:43 -0400

    tonights last styling push

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 window.onload = function() {
     fadeIn()
+    setTimeout(fadeIn2, 5000)
 }
   
 function fadeIn() {
     document.getElementById('fadeIn').style.opacity = '1';
+}
+
+function fadeIn2() {
+    document.getElementById('fadeIn2').style.opacity = '1';
 }
